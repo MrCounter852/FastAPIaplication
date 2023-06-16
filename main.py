97fccf5245cdf5eda6e6d6686e0ce47ed2e45612
MrCounter <johan321q@gmail.com>
2023-06-16 18:06:09 -0500

    arreglado identación

--- main.py
+++ main.py
@@ -21,20 +21,21 @@ def get_tasks():
 def create_tasks(task: Task):
     tasks.append(task)
     return task
-@app.delete("/tasks")
 
-def delete_tasks(task_id: int):
-    for tarea in tasks:
-        if tarea.id == task_id:
-            tasks.remove(tarea)
-            return {"message": "tarea eliminada"}
-        return {"message": "tarea no encontrada"}
 @app.put("/tasks/{task_id}")
- def update_task(task_id: int, task: Task):
+def update_task(task_id: int, task: Task):
     for tarea in task:
         if tarea.id == task_id:
             tarea.title = task.title
             tarea.description = task.description
             tarea.completed = task.completed
             return tarea
-    return {{{"message" : "tarea no encontrada"} 
+    return {"message" : "tarea no encontrada"} 
+             
+@app.delete("/tasks/{task_id}")
+def delete_tasks(task_id: int):
+    for tarea in tasks:
+        if tarea.id == task_id:
+            tasks.remove(tarea)
+            return {"message": "tarea eliminada"}
+    return {"message": "tarea no encontrada"}
